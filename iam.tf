a252f679261f26b83e30299d19e9cee233dc8d0c
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:50:24 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,52 +1,42 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "bedrock_lambda_role"
+resource "aws_iam_role" "lambda_exec" {
+  name = "bedrock_lambda_execution_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
       }
-      Action = "sts:AssumeRole"
-    }]
+    ]
   })
 }
 
-resource "aws_iam_policy" "bedrock_lambda_policy" {
+resource "aws_iam_policy" "lambda_policy" {
   name        = "bedrock_lambda_policy"
-  description = "Permissions pour la Lambda utilisant Bedrock"
+  description = "IAM policy for Lambda to access S3 and logs"
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect   = "Allow"
-        Action   = [
-          "bedrock:InvokeModel",
-          "bedrock:ListFoundationModels"
-        ]
-        Resource = "*"
+        Action   = ["s3:*"]
+        Resource = ["arn:aws:s3:::bedrock-state-files-*"]
       },
       {
         Effect   = "Allow"
-        Action   = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ]
-        Resource = "arn:aws:logs:eu-west-3:*:*"
+        Action   = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"]
+        Resource = ["arn:aws:logs:eu-west-3:*:*"]
       }
     ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_bedrock_policy" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.bedrock_lambda_policy.arn
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
 }
