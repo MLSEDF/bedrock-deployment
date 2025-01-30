33ecdb237a29c73d529f6887e7f87b5140e438df
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 09:33:54 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,58 @@
-resource "aws_bedrock_model" "ai_model" {
-  name                  = "my-bedrock-model"
-  foundation_model_name = "amazon.titan-text-lite-v1"
-  execution_role_arn    = aws_iam_role.bedrock_role.arn
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" },
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-output "model_id" {
-  value = aws_bedrock_model.ai_model.id
+resource "aws_iam_policy" "lambda_policy" {
+  name        = "lambda_policy"
+  description = "Policy pour permettre à Lambda d'utiliser Bedrock"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = "bedrock:InvokeModel"
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = "logs:*"
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.lambda_policy.arn
+}
+
+resource "aws_lambda_function" "bedrock_lambda" {
+  function_name = "invoke-bedrock"
+  role          = aws_iam_role.lambda_role.arn
+  runtime       = "python3.8"
+  handler       = "lambda_function.lambda_handler"
+
+  filename         = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
+
+  environment {
+    variables = {
+      BEDROCK_MODEL = "amazon.titan-text-lite-v1"
+    }
+  }
+}
+
+output "lambda_function_name" {
+  value = aws_lambda_function.bedrock_lambda.function_name
 }
