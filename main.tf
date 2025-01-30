e054db8a6f70a6273d4997f6c6dee0354cc49ad0
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:50:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,29 +1,20 @@
-resource "aws_iam_role" "lambda_role" {
-  name = "bedrock_lambda_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Effect = "Allow"
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-      Action = "sts:AssumeRole"
-    }]
-  })
+provider "aws" {
+  region = "eu-west-3"
 }
 
-resource "aws_iam_policy_attachment" "lambda_policy" {
-  name       = "lambda_basic_execution"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "bedrock-state-files-${var.account_id}"
 }
 
 resource "aws_lambda_function" "bedrock_lambda" {
-  function_name    = var.lambda_function_name
-  role            = aws_iam_role.lambda_role.arn
-  handler        = "lambda_function.lambda_handler"
-  runtime        = var.lambda_runtime
-  filename       = "lambda.zip"
-  source_code_hash = filebase64sha256("lambda.zip")
+  function_name    = "BedrockLambda"
+  role            = aws_iam_role.lambda_exec.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.9"
+  filename        = "lambda_function.zip"
+  source_code_hash = filebase64sha256("lambda_function.zip")
+}
+
+output "lambda_arn" {
+  value = aws_lambda_function.bedrock_lambda.arn
 }
