4f251a01d06d658d9dd80c0a38f222613b53fa21
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:12:26 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,57 +1,29 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
+  name = "bedrock_lambda_role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [{
-      Effect = "Allow",
-      Principal = { Service = "lambda.amazonaws.com" },
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_policy" "lambda_policy" {
-  name        = "lambda_policy"
-  description = "Policy pour permettre à Lambda d'utiliser Bedrock"
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect   = "Allow"
-        Action   = "bedrock:InvokeModel"
-        Resource = "*"
-      },
-      {
-        Effect   = "Allow"
-        Action   = "logs:*"
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_attach" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
+resource "aws_iam_policy_attachment" "lambda_policy" {
+  name       = "lambda_basic_execution"
+  roles      = [aws_iam_role.lambda_role.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_lambda_function" "bedrock_lambda" {
-  function_name = "invoke-bedrock"
-  role          = aws_iam_role.lambda_role.arn
-  runtime       = "python3.8"
-  handler       = "lambda_function.lambda_handler"
-
-  filename         = "lambda.zip"
-
-  environment {
-    variables = {
-      BEDROCK_MODEL = "amazon.titan-text-lite-v1"
-    }
-  }
-}
-
-output "lambda_function_name" {
-  value = aws_lambda_function.bedrock_lambda.function_name
+  function_name    = var.lambda_function_name
+  role            = aws_iam_role.lambda_role.arn
+  handler        = "lambda_function.lambda_handler"
+  runtime        = var.lambda_runtime
+  filename       = "lambda.zip"
+  source_code_hash = filebase64sha256("lambda.zip")
 }
