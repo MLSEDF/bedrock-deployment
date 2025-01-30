701ea10cc88ff7f13c1ce354c5636b3af99ca750
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:16:55 +0100

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -1,34 +1,52 @@
-resource "aws_iam_role" "bedrock_role" {
-  name = "bedrock_execution_role"
+resource "aws_iam_role" "lambda_role" {
+  name = "bedrock_lambda_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = { Service = "bedrock.amazonaws.com" }
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-resource "aws_iam_policy" "bedrock_policy" {
-  name        = "bedrock_policy"
-  description = "Policy pour utiliser Amazon Bedrock"
+resource "aws_iam_policy" "bedrock_lambda_policy" {
+  name        = "bedrock_lambda_policy"
+  description = "Permissions pour la Lambda utilisant Bedrock"
 
   policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect = "Allow",
-      Action = [
-        "bedrock:InvokeModel",
-        "bedrock:ListFoundationModels"
-      ],
-      Resource = "*"
-    }]
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "bedrock:InvokeModel",
+          "bedrock:ListFoundationModels"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Resource = "arn:aws:logs:eu-west-3:*:*"
+      }
+    ]
   })
 }
 
-resource "aws_iam_role_policy_attachment" "bedrock_attach" {
-  role       = aws_iam_role.bedrock_role.name
-  policy_arn = aws_iam_policy.bedrock_policy.arn
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_bedrock_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = aws_iam_policy.bedrock_lambda_policy.arn
 }
