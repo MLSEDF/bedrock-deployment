0b8e780e02a4f5cd1b35009902241dfe352bf844
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:11:13 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "bedrock-state-files"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+  }
+}
+
 provider "aws" {
   region = "eu-north-1"
 }
