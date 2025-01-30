7a8f6c4ee21f531060f435f6fa328245c2b027c6
MLSEDF <milo.le-saout@edf.fr>
2025-01-30 11:51:20 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,17 +1,4 @@
-variable "aws_region" {
-  description = "La région AWS où déployer les ressources"
+variable "account_id" {
+  description = "AWS Account ID"
   type        = string
-  default     = "eu-west-3"
-}
-
-variable "lambda_function_name" {
-  description = "Nom de la fonction Lambda"
-  type        = string
-  default     = "bedrock_lambda"
-}
-
-variable "lambda_runtime" {
-  description = "Runtime de la fonction Lambda"
-  type        = string
-  default     = "python3.9"
 }
